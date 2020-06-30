eb434acc0bb5921938801745e7afb5eb74fc2b9c
lesimoes <lesimoes0@gmail.com>
2020-06-29 23:30:59 -0300

    feat: some nice tests

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,30 @@
+import { 
+  AddAccount, 
+  AddAccountModel,
+  AccountModel, 
+  Encrypter, 
+  AddAccountRepository
+}  from './db-add-account-protocols';
+
+
+export class DbAddAccount implements AddAccount {
+
+  private readonly encrypter: Encrypter;
+  private readonly addAccountRepository: AddAccountRepository;
+
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
+    this.encrypter = encrypter;
+  }
+
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    const account = await this.addAccountRepository.add(Object.assign(
+      {},
+      accountData,
+      { password: hashedPassword }
+      ));
+    
+    return account;
+  }
+
+}
